8077a4b2242796e92b5575cfc548493a64006989
Harmanjot14 <jotkharman@gmail.com>
2025-07-21 02:51:32 +0000

    Added search() function

--- app/controllers/omdb.php
+++ app/controllers/omdb.php
@@ -6,13 +6,21 @@ class Omdb extends Controller{
     }
     public function search(){
         $movie_title = $_GET['movie'] ?? '';
+        
         if(empty($movie_title)){
             $this->view('home/index');
-            die;
+            return;
         }
         $api = $this->model('Api');
         $movie = $api->omdb_fetch($movie_title);
+        
+        if (!$movie || $movie['Response'] === 'False') {
+          $this->view('movie/index', ['error' => 'Movie not found.']);
+          return;
+        }
+        
         $this->view('movie/index',['movie' => $movie]);
+          
     }
   
 }
