2e76d3b935c4e4301548604a2e12397b6d721a62
Harmanjot14 <jotkharman@gmail.com>
2025-07-21 02:02:18 +0000

    No change

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -8,37 +8,37 @@
   <div class="page-container">
     <div class="page-header" id="banner">
         <div class="row">
-          <div>
-             <h1>Login</h1>
-          </div>
+            <div>
+               <h1>Login</h1>
+            </div>
          </div>
     </div>
 
-  <div class="row">
-    <div>
-      <form action="/login/verify" method="post" >
-        <div class="form-group">
-          <label for="username">Username:</label>
-          <br>
-          <input required type="text" class="form-control" name="username">
-          <br><br>
-        </div>
-        <div class="form-group">
-          <label for="password">Password:</label>
-          <br>
-          <input required type="password" class="form-control" name="password">
-          <br>
-        </div>
-          <br>
-          <button type="submit">Login</button>	
-      </form> 
-      <p>Don't have an account? <a href ="/create/index">Sign up</a></p>
+    <div class="row">
+      <div>
+        <form action="/login/verify" method="post" >
+          <div class="form-group">
+            <label for="username">Username:</label>
+            <br>
+            <input required type="text" class="form-control" name="username">
+            <br><br>
+          </div>
+          <div class="form-group">
+            <label for="password">Password:</label>
+            <br>
+            <input required type="password" class="form-control" name="password">
+            <br>
+          </div>
+            <br>
+            <button type="submit">Login</button>	
+        </form> 
+        <p>Don't have an account? <a href ="/create/index">Sign up</a></p>
+      </div>
+      <?php if (isset($_SESSION['error'])): ?>
+        <p style="color: red;"><?php echo $_SESSION['error']; ?></p>
+        <?php unset($_SESSION['error']);?>
+      <?php endif;?>
     </div>
-    <?php if (isset($_SESSION['error'])): ?>
-      <p style="color: red;"><?php echo $_SESSION['error']; ?></p>
-      <?php unset($_SESSION['error']);?>
-    <?php endif;?>
-  </div>
   </div>	
 </main>	
 
