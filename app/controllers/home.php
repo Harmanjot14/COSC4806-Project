b6227b04b62d9799f762bcab770895dfbd8e1393
Harmanjot14 <jotkharman@gmail.com>
2025-07-21 02:45:48 +0000

    Comment out gemini for now

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -8,6 +8,35 @@ class Home extends Controller {
 
       $this->view('home/index');
       die;
-    }
+      /*
+      $url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=".$_ENV['GEMINI_KEY'];
+      $data = array(
+        "contents" => array(
+          array(
+            "parts" => array(
+              array(
+                "text" => $text
+              )
+            )
+          )
+        )
+      );
+      
+      $json_data = json_encode($data);
+      $ch = curl_init($url);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+      $response = curl_exec($ch);
+      curl_close($ch);
+      if(curl_errno($ch)){
+        echo 'Curl error: ' . curl_error($ch);
+      }
+
+      echo "<pre>";
+      echo $response;
+      die; */
+      }
 
 }
