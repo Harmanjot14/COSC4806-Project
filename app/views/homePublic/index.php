1fcf695e6e4cc33299d5462341345213e82a32a7
Harmanjot14 <jotkharman@gmail.com>
2025-07-21 03:29:07 +0000

    Added search bar and button, with movie info

--- app/views/homePublic/index.php
+++ app/views/homePublic/index.php
@@ -6,17 +6,41 @@
             <div class="row">
                 <div>
                     <h1>Welcome!</h1>
-                    <p class="lead">This is a movie review website</p>
-                    <form method="Get" action="/omdb/search">
-                        <input type="text" name="movie" placeholder="Search for a movie">
+                    <form method="Get" action="/homePublic/search">
+                        <input type="text" name="movie" class="search-input" placeholder="Search for a movie">
                         <br><br>
-                        <input type="submit" value="Search">
+                        <input type="submit" class="search-button" value="Search">
                         <br><br>
                     </form>
-                    <p class="lead"><strong><?= date("F jS, Y"); ?></strong> </p>
+                    
                 </div>
+                <br>
+                <hr>
+
+                 <?php if(isset($movie) && $movie['Resposnive'] !== 'False'): ?>
+                    <!--Movie Information-->
+                        <p>Movie Information:</p>
+                            <div class="card-body">
+                                <h2 class="card-title"><?= $movie['Title'] ?> (<?= $movie['Year'] ?>)</h2>
+                                <img src="<?= $movie['Poster'] ?>" alt="Movie Poster">
+
+                                <p><strong>Plot: </strong><?= $movie['Plot']?></p>
+                                <p><strong>Language: </strong><?= $movie['Language']?></p>
+
+                                <p><strong>IMDB Rating: </strong><?= $movie['imdbRating']?></p>
+                            </div>
+                            <a href="/login">For More information Login to account</a>
+                    <br><br><br><br>
+                    <p class="lead"><strong><?= date("F jS, Y"); ?></strong> </p>
+                    
+    
+                <?php elseif(isset($error)): ?>
+                    <div class="alert alert-warning text-center">
+                       <?= $error ?>
+                    </div>
+                <?php endif; ?>
             </div>
-        </div>
+        </div>       
     </div>
 </div>
 
