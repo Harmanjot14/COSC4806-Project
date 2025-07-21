d61cbcb20fb1169a2cc8d56a87017cbdf402420e
Harmanjot14 <jotkharman@gmail.com>
2025-07-21 03:28:45 +0000

    Added search function for non-logged in users

--- app/controllers/homePublic.php
+++ app/controllers/homePublic.php
@@ -4,7 +4,24 @@ class HomePublic extends Controller {
 
     public function index() {
       $this->view('homePublic/index');
-      die;
+    }
+    public function search(){
+        $movie_title = $_GET['movie'] ?? '';
+
+        if(empty($movie_title)){
+            $this->view('homePublic/index');
+            return;
+        }
+        $api = $this->model('Api');
+        $movie = $api->omdb_fetch($movie_title);
+
+        if (!$movie || $movie['Response'] === 'False') {
+          $this->view('homePublic/index', ['error' => 'Movie not found.']);
+          return;
+        }
+
+        $this->view('homePublic/index',['movie' => $movie]);
+
     }
 
 }
