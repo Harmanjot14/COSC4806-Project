6f71e851bd9ce7aead66770585739b8808c3a3b7
Harmanjot14 <jotkharman@gmail.com>
2025-07-22 02:57:35 +0000

    Pasted Gemini part from home page comment to the API model function call_gemini

--- app/models/Api.php
+++ app/models/Api.php
@@ -12,6 +12,35 @@ class Api{
 
     //for gemini api
     public function call_gemini($movie_title){
-      
+        $url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=".$_ENV['GEMINI_KEY'];
+        $text = "Write a few sentences review about the movie with movie release date and people opinion " . $movie_title;
+        $data = array(
+            "contents" => array(
+              array(
+                "parts" => array(
+                  array(
+                    "text" => $text
+                  )
+                )
+              )
+            )
+        );
+
+        $json_data = json_encode($data);
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $response = curl_exec($ch);
+        
+        if(curl_errno($ch)){
+          echo 'Curl error: ' . curl_error($ch);
+        }  
+        curl_close($ch);
+        $json = json_decode($response, true);
+        return $json ['candidates'][0] ['content']['parts'][0]['text'] ?? 'Gemini could not provide a review' ;
     }
+
+    
 }
