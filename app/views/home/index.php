173941766b7c0071c4c8a5c2cead0152905beee0
Harmanjot14 <jotkharman@gmail.com>
2025-07-22 04:21:34 +0000

    Modified look of home page

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -4,12 +4,12 @@
         <div class="page-container">
             <div class="page-header" id="banner">
                 <div class="row">
-                    <div>
-                        <br>
-                        <h1 style="font-size: 24px;">Welcome!</h1>
+                    <div class="home-wrapper text-center">
+                        <br><br><br><br><br><br><br>
+                        <h1 style="font-size: 32px;">Welcome!</h1>
                         <br>
                         <form method="Get" action="/omdb/search">
-                            <input type="text" name="movie" placeholder="Search for a movie">
+                            <input type="text" name="movie" placeholder="Search for a movie" style="width: 400px;">
                             <input type="submit" value="Search">
                             <br><br>
                         </form>
