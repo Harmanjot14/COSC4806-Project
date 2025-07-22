f45b9f44bf7e1285c3f1841680377c78e9a10ac6
Harmanjot14 <jotkharman@gmail.com>
2025-07-22 00:22:24 +0000

    Removed return statement

--- app/models/MovieRatings.php
+++ app/models/MovieRatings.php
@@ -9,6 +9,5 @@ Class MovieRatings{
       $db = db_connect();
       $statement = $db->prepare("INSERT INTO ratings (movie_title, user_rating, user_id) VALUES (:movie_title, :user_rating, :user_id);");
       $statement->execute(['movie_title' => $movie_title, 'user_rating' => $user_rating, 'user_id' => $user_id]);
-      return "Movie rated successfully";
    }
 }
