7212fcbf0ed7cec51f48e4e66ef50531b3c6204c
Harmanjot14 <jotkharman@gmail.com>
2025-07-22 00:19:15 +0000

    Added function rate()

--- app/controllers/movie.php
+++ app/controllers/movie.php
@@ -5,6 +5,18 @@ class Movie extends Controller{
     public function index(){
         $this->view('movie/index');
     }
+    
+    public function rate(){
+        $movie_title = $_POST['movie_title'];
+        $user_rating = $_POST['user_rating'];
+        $user_id = $_SESSION['user_id'];
+        
+        $movieRate = $this->model('MovieRatings');
+        $movieRate->rate_movie($movie_title, $user_rating, $user_id);
+        
+        header("Location: /movie/index");
+        exit;
+    }
 
   
     
