bd7027ca35b51b2004d1716e4238de6b86401789
Harmanjot14 <jotkharman@gmail.com>
2025-07-22 00:53:15 +0000

    Added urlencode

--- app/models/Api.php
+++ app/models/Api.php
@@ -4,7 +4,7 @@ class Api{
 
     //for omdb api
     public function omdb_fetch($movie_title){
-        $url = "http://www.omdbapi.com/?apikey=".$_ENV['omdb_key']."&t=".$movie_title;
+        $url = "http://www.omdbapi.com/?apikey=".$_ENV['omdb_key']."&t=".urlencode($movie_title);
         $json = file_get_contents($url);
         $movie = json_decode($json, true);
         return $movie;
